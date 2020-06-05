5b0cb3e1b8b32afdafa12825b7077cb09d7e8d26
ZHAP082 <ZHAP082@live.rhul.ac.uk>
2020-06-05 11:57:26 +0000

    Plotting a function

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 datapoints = 100
 x = np.arange(datapoints)
-y = np.sin(2 * np.pi * x/datapoints)
+y = x**3 - (100*x**2) + x + 1
 plt.plot(x,y)
-plt.xlabel('datapoints')
-plt.ylabel('sin(2$\pi$ datapoints)')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
