4193d1af7065bddfe0039f360db21ed5baaf6587
ZHAP082 <ZHAP082@live.rhul.ac.uk>
2020-06-05 17:22:23 +0000

    Another plot

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-datapoints = 100
-x = np.arange(datapoints)
-y = x**3 - (100*x**2) + x + 1
-plt.plot(x,y)
-plt.xlabel('x')
-plt.ylabel('y')
+x = np.array([1,2,3,4,5,6])
+y = np.array([10,100,1000,10000,100000,1000000])
+y = np.log10(y)
+plt.plot(x,y,'ro')
+plt.xlabel('Pressure/Pa')
+plt.ylabel('Volume/m$ˆ3$')
+plt.axis([0,7,0,7])
 plt.show()
+
