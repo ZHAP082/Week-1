c8a1dc9058991541c25c84232f07acd32275fdaf
ZHAP082 <ZHAP082@live.rhul.ac.uk>
2020-06-05 20:17:15 +0000

    array for radias

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.array([1,2,3,4,5,6])
-y = np.array([10,100,1000,10000,100000,1000000])
-y = np.log10(y)
-plt.plot(x,y,'ro')
-plt.xlabel('Pressure/Pa')
-plt.ylabel('Volume/m$ˆ3$')
-plt.axis([0,7,0,7])
-plt.show()
 
+
+f = open('Galaxy1 (4).txt')
+f.readline()
+
+radias = []
+
+for line in f:
+  radias.append(float(line.split('\t')[0]))
+
+x = np.array(radias)
+
+print (x)
