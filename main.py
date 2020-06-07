4770e71b3e984cad27dd1e40e16c66e93ed6050c
ZHAP082 <ZHAP082@live.rhul.ac.uk>
2020-06-07 08:32:09 +0000

    Example of getting data for specific thing from file so you can make a array of it.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import numpy as np
-import matplotlib.pyplot as plt
+f = open('Galaxy1 (4).txt','r')#open file in read mode
+list_make = f.readlines()#reads file and makes it into a list
+
+All_data = []#makes empty list Main
+Radias  = []#makes empty list Radias
+
+for line in list_make:#iterates through the file which we made a list
+    make_each_list = line.split('\t')#split the list making each line its own list
+    All_data.append(make_each_list)#adds to the list All_data each line. This makes it a list with multiple lists each a line
 
 
-f = open('Galaxy1 (4).txt')
-f.readline()
 
-radias = []
+for i in All_data:#iterates through All_data
+    Radias.append(i[0])#Adds the element 0 from each line aka the radias from All_data data
+    
 
-for line in f:
-  radias.append(float(line.split('\t')[0]))
-
-x = np.array(radias)
-
-print (x)
+Radias_data = (Radias[1:])#Radias list along with the radias data includes the title. We only want data so we use slicing to get rid of first elemnent which is the title.
